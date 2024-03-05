76a6eceafc43dd58c98886ba8b9811a9facc15ff
Quentin Berling <quebe630@student.liu.se>
2024-03-05 18:01:45 +0100

    Fix localhost / 127.0.0.1 issue

--- server/static/js/server.js
+++ server/static/js/server.js
@@ -39,7 +39,7 @@ const server = {
 	},
 
 	signIn: async (email, password) => {
-		return sendRequest('GET', 'http://localhost:5000/sign_in/', {
+		return sendRequest('GET', 'http://127.0.0.1:5000/sign_in/', {
 			username: email,
 			password: password,
 		});
